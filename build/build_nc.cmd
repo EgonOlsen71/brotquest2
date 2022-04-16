d2f2f2bddbe47e0c4249cdc9242fb1f0548eba96
EgonOlsen <info@jpct.net>
2022-04-17 00:14:56 +0200

    Init

---
+++ build/build_nc.cmd
@@ -0,0 +1,12 @@
+del brotquest2_AD.d64
+c1541 -format brotquest2,bq d64 brotquest2_AD.d64
+call ..\build\c1541 ..\build\brotquest2_AD.d64 -write ++brotquest2.prg brotquest2,p
+call ..\build\c1541 ..\build\brotquest2_AD.d64 -write ++xam-c.prg xam,p
+cd ..\build
+cd ..\seq
+for %%f in (*.*) do call :add "%%f"
+cd ..\build
+goto :EOF
+
+:add
+..\build\c1541 ..\build\brotquest2_AD.d64 -write %1 %1,s
