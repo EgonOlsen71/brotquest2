2dcbd6475adaebbe2a609212f2dbdc2a1437e57d
EgonOlsen <info@jpct.net>
2022-04-22 10:48:56 +0200

    Init

--- build/compile.cmd
+++ build/compile.cmd
@@ -1,2 +1,2 @@
-call mospeed ..\basic\xam.bas -compactlevel=3 -compression=true -generatesrc=true
+call mospeed ..\basic\xam.bas -compactlevel=3 -compression=true -generatesrc=false
 call mospeed ..\basic\brotquest2.bas -compactlevel=5
